45b2cff1c73813f3129e4523851e046d1617093f
Paul Bennett <pbennett.uk@outlook.com>
2026-08-14 18:25:06 +0100

    nvim config tweaks

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,15 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup.
 require("config.remote_clipboard").setup()
 
-vim.opt.relativenumber = true
-vim.g.autoformat = true
-
 vim.opt.mouse = "a"
 vim.opt.swapfile = false
+vim.opt.autoread = true
 
-vim.opt.formatoptions:remove({"c", "r", "o"})
+-- stop auto comments
+vim.opt.formatoptions:remove({ "c", "r", "o" })
 
+-- python lazyvim
 vim.g.lazyvim_python_lsp = "ty"
 vim.g.lazyvim_python_ruff = "ruff"
 
+-- disable the option to require prettier config file
 vim.g.lazyvim_prettier_needs_config = false
